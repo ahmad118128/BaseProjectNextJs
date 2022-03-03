ba085ce9174b84d9a75cf427bde6dedb8d18f532
ahmad safari <sargashte118@gmail.com>
2022-03-03 20:57:22 +0330

    setup mui 5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+
+import createEmotionCache from 'src/styles/createEmotionCache';
+import 'src/styles/globals.css';
+import theme from 'src/styles/theme';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider value={clientSideEmotionCache}>
+      <Head>
+        <title>AIBOX PRO</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
