ea97b933d64a3d24ca37c76f384acf6156597371
ahmad safari <sargashte118@gmail.com>
2022-03-06 08:02:40 +0330

    setup storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,40 +3,28 @@ const config = {
     ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   env: {
     browser: true,
     node: true,
-    es6: true,
+    es6: true
   },
   plugins: ['simple-import-sort'],
   settings: {
     react: {
-      version: 'latest',
-    },
+      version: 'latest'
+    }
   },
-  extends: [
-    'next',
-    'next/core-web-vitals',
-    'eslint:recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'plugin:sonarjs/recommended',
-    'plugin:unicorn/recommended',
-    'plugin:security/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
+  extends: ['next', 'next/core-web-vitals', 'eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:react/recommended', 'plugin:prettier/recommended', 'plugin:sonarjs/recommended', 'plugin:unicorn/recommended', 'plugin:security/recommended', 'plugin:react-hooks/recommended', 'prettier'],
   rules: {
     'no-console': 'error',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     'simple-import-sort/sort': 'off',
-    'unicorn/filename-case': 'off',
-  },
+    'unicorn/filename-case': 'off'
+  }
 };
-
 export default config;
+export const extends = ["plugin:storybook/recommended"];
